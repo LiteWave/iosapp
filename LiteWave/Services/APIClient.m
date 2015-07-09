b129f8455cb50ce9ab235ed1597183b44912c192
David Anderson <davidmartinanderson@gmail.com>
2015-07-08 23:26:05 -0700

    cleanup

--- LiteWave/Services/APIClient.m
+++ LiteWave/Services/APIClient.m
@@ -6,14 +6,6 @@
 //  Copyright (c) 2013 LightWave. All rights reserved.
 //
 
-#define API_URL = @"http://127.0.0.1:3000/api"
-#define API_getClients = @"/clients"
-#define API_getClientEvents = @"/{clientid}/lw_events"
-#define API_postEventUserLocation = @"/lw_events/{eventid}/user_locations"
-#define API_postEventJoin = @"/user_locations/{userid}/event_joins"
-#define API_getEventShows = @"/lw_events/{eventid}/event_liteshows"
-#define API_getUserEventShow = @"/event_liteshows/{showid}/user_locations/{userid}/liteshow"
-
 #import "APIClient.h"
 #import "AFNetworking.h"
 #import "Configuration.h"
